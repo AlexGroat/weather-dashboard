f7891f3eff5ce22047321b321a8e5ae5a9b4c8c5
Alex Groat <alex.groat.95@gmail.com>
2021-10-16 19:47:20 +1030

    Fifth commit added list appended by js and started js for city history

--- Assets/script.js
+++ Assets/script.js
@@ -4,9 +4,26 @@ var city =  "";
 var searchCity = $("#search-city");
 var searchCityButton = $("#search-city-button");
 
+// API key for openweathermap.org
+var apiKey = "1514b2ce008df16fa808e34044020461"
+
 // weather is displayed when user clicks on search button 
 searchCityButton.on("click", weatherDisplay);
 
+// after running below function it will display weather 
 function weatherDisplay(event) {
+    event.preventDefault();
+    if (searchCity.val().trim() !== "") {
+        city - searchCity.val().trim();
+        currentWeather(city);
+
+
+// store previously searched cities in local storage 
+
+        var listedCity = document.getElementById("city-history");
+        cityHistory.textContent = "";
+
+        
     
+    }
 }
